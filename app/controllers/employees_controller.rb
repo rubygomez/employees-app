8b1af2b6382386d2359a93d92c6808311cdf8bb8
rubygomez <rvbii.gomez@gmail.com>
2024-03-07 12:08:01 -0800

    validations and happy/sad paths for create and update

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -17,7 +17,11 @@ class EmployeesController < ApplicationController
             title: params[:title],
             department_id: params[:department_id]
         )
-        render :show
+        if @employee.valid?
+            render :show
+        else
+            render json: {errors: @employee.errors.full_messages}, status: 422
+        end
     end
 
     def update
@@ -30,12 +34,16 @@ class EmployeesController < ApplicationController
             title: params[:title] || @employee.title,
             department_id: params[:department_id] || @employee.department_id
         )
-        render :show
+        if @employee.valid?
+            render :show
+        else
+            render json: {errors: @employee.errors.full_messages}, status: 422
+        end
     end
 
     def destroy
         @employee = Employee.find_by(id: params[:id])
-        @employee.delete
+        @employee.destroy
         render json: {message: "Successfully deleted"}
     end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name_name, presence: true
+    validates :email, uniqueness: presence: true
+    validates :phone, presence: true
+    validates :title, presence: true
+    validates :department_id, presence: true
     belongs_to :department
 end
