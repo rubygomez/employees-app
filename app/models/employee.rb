cc6930c0e5f9fb439ed7a1a594403046d8dc2985
rubygomez <rvbii.gomez@gmail.com>
2024-03-07 18:38:42 -0800

    tests

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
     validates :first_name, presence: true
-    validates :last_name_name, presence: true
-    validates :email, uniqueness: presence: true
+    validates :last_name, presence: true
+    validates :email, uniqueness:true, presence: true
     validates :phone, presence: true
     validates :title, presence: true
     validates :department_id, presence: true
--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -4,4 +4,44 @@ class EmployeesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "create" do
+    assert_difference "Employee.count", 1 do
+      post "/employees.json", params: {first_name: "test", last_name: "test", email: "email", phone:"34232322", title:"test", department_id: Department.first.id}
+      assert_response 200
+    end
+    assert_difference "Employee.count", 0 do
+      post "/employees.json", params: {first_name: "test", last_name: "test", email: "email", phone:"34232322", title:"test", department_id: Department.first.id}
+      assert_response 422
+    end
+  end
+
+  test "index" do
+    get "/employees.json"
+    assert_response 200
+    data = JSON.parse(response.body)
+    assert_equal Employee.count, data.length
+  end
+
+  test "show" do
+    employee = Employee.first
+    get "/employees/#{Employee.first.id}.json"
+    assert_response 200
+    data = JSON.parse(response.body)
+    assert_equal employee.first_name, data["first_name"]
+  end
+
+  test "destroy" do
+    assert_difference "Employee.count", -1 do
+      delete "/employees/#{Employee.first.id}.json"
+      assert_response 200
+    end
+  end
+
+  test "update" do
+    employee = Employee.first
+    patch "/employees/#{employee.id}.json", params: {email: "Bob"}
+    assert_response 200
+    updated_employee = Employee.find(employee.id)
+    assert_equal "Bob" , updated_employee.email
+  end
 end
